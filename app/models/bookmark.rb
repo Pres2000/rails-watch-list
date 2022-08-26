b6e0ca93e9c7dbd4fd857f3e6658291a2a346249
Preston Hogaboam <pjhogaboam@gmail.com>
2022-08-26 16:04:11 +0200

    add dotenv-rails

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,5 +3,5 @@ class Bookmark < ApplicationRecord
   belongs_to :list
 
   validates :comment, length: 6..150, allow_blank: false
-  validates :movie_id, uniqueness: { scope: :list_id }
+  validates :movie_id, uniqueness: { scope: :list_id, message: 'is already in the list' }
 end
